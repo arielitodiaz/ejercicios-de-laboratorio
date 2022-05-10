f2e5bcb3b77b1c21caf9c7cb42c90e7a46e523eb
arielitodiaz <100837288+arielitodiaz@users.noreply.github.com>
2022-05-10 07:28:15 -0300

    Add files via upload

---
+++ cuestionario for  ciclos prueba/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int  n ,c;
+    cin >> n;
+    for(int i=1; i<=10; i++){
+
+        if(n>0){
+            c++;
+        }
+    }
+    cout << n;
+    return 0;
+}
